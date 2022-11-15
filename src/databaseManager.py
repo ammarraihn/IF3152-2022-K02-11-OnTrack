660bb6f0ff1836734833df28a3c4e5ec30e539b3
ammarraihn <ammarraihan00@gmail.com>
2022-11-15 18:14:09 +0700

    add template databaseManager.py

---
+++ src/databaseManager.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+class databaseManager():
+
+    def __init__(self):
+        pass
+
+    def addToDb(self, activityName, category, deadline):
+        pass
+
+    def delInDb(self, vals):
+        pass
+
+    def completeInDb(self, vals):
+        pass
+
+    def selectOngoingDb(self):
+        pass
+
+    def selectIdleDb(self):
+        pass
+
+    def selectCompletedByCategoryDb(self, category):
+        pass
