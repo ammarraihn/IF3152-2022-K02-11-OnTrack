41c8be32302cd017bbc1be97c3bc817f1e38f25b
FarisTF <faris.tristan@gmail.com>
2022-11-16 20:41:04 +0700

    Add delete by id database query

--- src/databaseManager.py
+++ src/databaseManager.py
@@ -8,8 +8,15 @@ class databaseManager():
     def addToDb(self, activityName, category, deadline):
         pass
 
-    def delInDb(self, vals):
-        pass
+    def delInDb(self, val):
+        mydb = mysql.connector.connect(host = "localhost", user = "root", password = "qwerty123", database = "ontrack", auth_plugin = "mysql_native_password")
+        cursor = mydb.cursor()
+
+        if (val != ''):
+            sql = "DELETE FROM List_of_Activities WHERE ActivityID = %s"
+            cursor.execute(sql, [val[0]])
+        mydb.commit()
+        mydb.close()
 
     def completeInDb(self, vals):
         pass
